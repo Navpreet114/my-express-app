cbbd9816c528e843b0e7668c2805ab3c7381db3f
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-11-18 10:22:42 -0700

    Update

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//in the brouser http:localhost:3000/user/23
+
 app.get("/user/:id", (req, res) => {
     const userId = req.params.id;
     res.send(`User id is ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+    const query = req.query.q;
+    res.send(`The search term is ${query}`);
 });
 
 app.listen(PORT, () => {
